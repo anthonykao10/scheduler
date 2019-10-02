54188e2552a1c54824d3e3a0d06af8b426811853
anthony <anthonykao10@gmail.com>
2019-10-02 14:33:02 -0700

    add getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,14 @@
+/**
+ * Returns array of objects with appointment details
+ * @param {object} state 
+ * @param {string} day 
+ * @return {array}
+ */
 export function getAppointmentsForDay(state, day) {
   const dayObj = state.days.find(elem => elem.name === day);
   const output = [];
   if (!dayObj) return [];
-  
+
   const appointments = dayObj.appointments;
   appointments.map(appointment => {
     if (state.appointments[appointment]) {
@@ -10,4 +16,18 @@ export function getAppointmentsForDay(state, day) {
     }
   });
   return output;
+}
+
+/**
+ * @param {object} state 
+ * @param {object} interview
+ * @return {object}
+ */
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  const interviewer = state.interviewers[interview.interviewer];
+  return {
+    ...interview,
+    interviewer
+  };
 }
