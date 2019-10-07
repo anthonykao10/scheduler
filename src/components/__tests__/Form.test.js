d470f27ae6b60cf74b39cc7d15a5c58f0bad1978
anthony <anthonykao10@gmail.com>
2019-10-07 13:47:49 -0700

    cleanup form tests

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -16,25 +16,29 @@ describe("Form", () => {
   ];
 
   it("renders without student name if not provided", () => {
-    const { getByPlaceholderText } = render(<Form interviewers={interviewers}/>);
+    const { getByPlaceholderText } = render(
+    <Form interviewers={interviewers}/>
+    );
+
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
   it("renders with initial student name", () => {
-    const { getByTestId } = render(<Form name="Lydia Miller-Jones" interviewers={interviewers}/>);
+    const { getByTestId } = render(
+    <Form name="Lydia Miller-Jones" interviewers={interviewers}/>
+    );
+
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
   it("validates that the student name is not blank", () => {
     const onSave = jest.fn();
   
-    const { debug, getByText } = render(
+    const { getByText } = render(
       <Form onSave={onSave} interviewers={interviewers} />
     );
   
-    
     fireEvent.click(getByText(/save/i));
-    // debug();
     
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
