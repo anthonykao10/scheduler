3a037e639ba1d49a79b6f8cad80645140bc08da5
anthony <anthonykao10@gmail.com>
2019-10-07 22:50:47 -0700

    add tests for editing appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -96,4 +96,39 @@ describe("Application", () => {
     // Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
+
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // Select booked appointment
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    
+    // Edit appointment
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // Check for "Status" component
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // Check for student name, after "Saving" indicator is hidden
+    await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+
+    // Verify spots don't change
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
 });
