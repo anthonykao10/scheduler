fe52e6a59677ed2d54a4276aee2fd39ed8be5bfc
anthony <anthonykao10@gmail.com>
2019-10-08 08:20:29 -0700

    separate reducer into its own file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,57 +1,12 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
 
-const SET_DAY = "SET_DAY";
-const UPDATE_SPOTS = "UPDATE_SPOTS";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { 
-        ...state, 
-        day: action.day 
-      }
-    case SET_APPLICATION_DATA:
-      return { 
-        ...state, 
-        days: action.days, 
-        appointments: action.appointments, 
-        interviewers: action.interviewers 
-      }
-    case SET_INTERVIEW: {
-      return { 
-        ...state, 
-        appointments: action.appointments 
-      }
-    }
-    case UPDATE_SPOTS: {
-      const newDays = [...action.days];
-      const dayObj = newDays.find(dayObj => dayObj.name === action.day);
-
-      // Get spots for day
-      let spots = 0;
-      dayObj.appointments.forEach(appointmentId => {
-        // Increment spots if interview obj exists for that appointment
-        if (!state.appointments[appointmentId].interview) spots++;
-      });
-      // Update spots
-      newDays[dayObj.id - 1].spots = spots;
-      
-      return {
-        ...state,
-        days: newDays
-      }
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
-
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  UPDATE_SPOTS
+} from "reducers/application";
 
 export default function useApplicationData() {
   const [ state, dispatch ] = useReducer(reducer, 
