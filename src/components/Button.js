eac02f03157c98aa04e7436786ec573aef83dd3c
anthony <anthonykao10@gmail.com>
2019-10-09 08:33:30 -0700

    remove unnecessary comments for Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,24 +4,6 @@ import "components/Button.scss";
 import classnames from 'classnames';
 
 export default function Button(props) {
-   // let buttonClass = "button";
- 
-   // if (props.confirm) {
-   //   buttonClass += " button--confirm";
-   // }
- 
-   // if (props.danger) {
-   //   buttonClass += " button--danger";
-   // }
-
-   /* refactor with classnames */
-   // let buttonClass = classnames({
-   //    'button': true,
-   //    'button--confirm': props.confirm && true,
-   //    'button--danger': props.danger && true
-   // });
-
-   /* sample solution */
    let buttonClass = classnames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
